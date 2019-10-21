cefb8a780e727c24719202dcc2dced6522bf7517
Richie Bendall <richiebendall@gmail.com>
2019-10-21 16:24:25 +1300

    chore: Update meta
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from "ava"
 import isWritable from "."
 
-test("main", (t) => {
+test("main", async (t) => {
     t.true(isWritable.sync(__dirname))
-    isWritable.async(__dirname).then(t.true)
+    t.true(await isWritable.async(__dirname))
 })
